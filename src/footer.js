f3f2da087ac8ad4959b090b075ab27a379cacf84
mithup15 <96760114+mithup15@users.noreply.github.com>
2022-05-06 00:09:36 +0530

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+function footer(){
+    return(
+        <div id="footer">
+            <h2>FOOTER</h2>
+        </div>
+    )
+}
+export default footer;
---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function header(){
+    return(
+        <div id="header">
+            <h2>HEADER</h2>
+        </div>
+    )
+}
+export default header;
